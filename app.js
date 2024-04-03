417f77462a035c93c51e55ac97b0329dd8b696cd
Denperidge <denperidge@gmail.com>
2024-04-03 17:26:12 +0200

    Implemented sass/scss

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sassMiddleware = require("node-sass-middleware");
 
 var indexRouter = require('./app/routes');
 
@@ -17,6 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, "styles"),
+  dest: path.join(__dirname, "public", "stylesheets"),
+  outputStyle: "compressed",
+  prefix: "/stylesheets/"
+}));
 
 app.use('/', indexRouter);
 
