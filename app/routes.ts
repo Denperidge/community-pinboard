24e91493ad4fd4f1e735a05b99d660fa515fdb25
Denperidge <denperidge@gmail.com>
2024-04-07 16:51:58 +0200

    Improved form handling, bugfix, code cleanup
    Fixed pinPreview background-image failing if title contains spaces
    
    This progresses #2

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { check, validationResult, matchedData, ValidationError, FieldValidationError } from "express-validator";
+import { check, validationResult, matchedData, FieldValidationError } from "express-validator";
 
 import { UPLOADS_DIR, PUBLIC_UPLOADS_PATH } from "./conf";
 import * as db from "./db";
@@ -8,28 +8,6 @@ import multer from "multer";
 
 
 const router = express.Router();
-
-/*
-const storage = multer.diskStorage({
-  // Adapted from https://www.npmjs.com/package/multer#diskstorage
-  destination: (req, file, cb) => {
-    cb(null, "data/uploads/");
-  },
-  filename: (req, file, cb) => {
-    //const name = matchedData(req).title;
-    //const extension = file.originalname.substring(file.originalname.lastIndexOf("."))
-    /*
-    TODO: this can cause overwriting!
-    So fix that sometime.
-
-    This system also saves to disk when the forms aren't completed.
-    All round not production ready, but it works 
-    */
-/*    cb(null, file.originalname);
-  }
-})
-*/
-
 const upload: ReturnType<typeof multer> = multer({storage: multer.memoryStorage()});
 // See https://github.com/DefinitelyTyped/DefinitelyTyped/issues/41970
 
@@ -52,22 +30,45 @@ router.post(
   "/new-event", 
   [
     upload.single("thumbnailFile"),
-    check("title").notEmpty(),
-    check("description"),
-    check("location").notEmpty(),
-    check("datetime").notEmpty(),
-    check("postedBy").notEmpty(),
-    check("thumbnailUrl"),
-    check("thumbnailFile")
+    check("title")
+      .notEmpty().withMessage("The title must not be empty")
+      .trim()
+      .isLength({max: 20}).withMessage("The title has to be 20 characters or shorter"),
+    check("description").optional()
+      .isLength({max: 200})
+      .withMessage("The description has to be 200 characters or shorter"),
+    check("location")
+      .notEmpty()
+      .withMessage("The location needs to be filled in")
+      .isLength({max: 50})
+      .withMessage("The location has to be 50 characters or shorter"),
+    check("datetime")
+      .notEmpty()
+      .withMessage("A date/time has to be provided")
+      // TOdo: regex based date format?
+      //.isDate()
+      //.withMessage("Provided date/time incorrect format")
+      ,
+    check("postedBy")
+      .notEmpty()
+      .withMessage("Posted by cannot be empty")
+      .isLength({max: 20})
+      .withMessage("Posted by has to be 20 characters or shorter"),
+    check("thumbnailUrl").optional(),
+    check("thumbnailFile").optional()
   ],
   async function(req: express.Request, res: express.Response, next: express.NextFunction) {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
+      console.error("Errors")
+      console.error(errors)
       const returnErrors: {[key:string]: string} = {};
       (errors.array() as FieldValidationError[]).forEach((err: FieldValidationError) => {
         returnErrors[err.path] = `${err.msg}: "${err.value}"`;
       });
+
+      // TODO: go to #new on load
       res.render("index", {
         pinArray: await db.getPins(),
         errors: returnErrors,
@@ -77,17 +78,11 @@ router.post(
 
 
     const pinData = matchedData(req);
-    console.log("thumbnailurl = '" + pinData.thumbnailUrl + "'")
-    console.log(req.file)
-    console.log(pinData.thumbnailFile)
     if (pinData.thumbnailUrl) {
-      console.log("222ZDSFGFDS")
       pinData.thumbnail = pinData.thumbnailUrl;
     } else if (req.file) {
       const extension = req.file.originalname.substring(req.file.originalname.lastIndexOf("."))
       const na = await db.saveImage(pinData.title + extension, req.file?.buffer)
-      console.log("333")
-      console.log(na)
       pinData.thumbnail = na;
     }
 
