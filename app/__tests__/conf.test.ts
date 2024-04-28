880b6b535ff1eac718ca1070ed25caa1858ae4b5
Denperidge <denperidge@gmail.com>
2024-04-28 13:49:16 +0200

    WIP conf.test

---
+++ app/__tests__/conf.test.ts
@@ -0,0 +1,34 @@
+import { _DEFAULTS } from "../conf";
+
+const envVarNames = Object.keys(_DEFAULTS);
+
+function setEnvVars(setUndefined=false) {
+    const values: {[key:string]: string|number|undefined} = _DEFAULTS;
+    if (setUndefined) {
+        envVarNames.forEach((name: string) => {
+            values[name] = undefined;
+        });
+    }
+    envVarNames.forEach((name: string) => {
+        const setTo = values[name];
+        process.env[name] = setTo?.toString();
+    });
+}
+
+
+test("conf - default values when unassigned", () => {
+    // setUndefined true
+    setEnvVars(true);
+
+    const expectedValues: {[key:string]: string|number} = _DEFAULTS;
+
+    const conf = require("../conf");
+
+    envVarNames.forEach((name: string) => {
+        expect(conf[name]).toBe(expectedValues[name])
+    })
+});
+
+test("conf - assign through environment variables", () => {
+    
+});
--- app/conf.ts
+++ app/conf.ts
@@ -1,21 +1,46 @@
 import { config } from "dotenv";
-config()
 
-export const HOST_DOMAIN = process.env.HOST_DOMAIN || "localhost:3000";
-export const DATA_DIR = process.env.DATA_DIR || "data/";
-export const WEBSITE_TITLE = process.env.WEBSITE_TITLE || "Community Pinboard!";
-export const WEBSITE_DESCRIPTION = process.env.WEBSITE_DESCRIPTION || "A public event pinboard for your local community!";
-export const WEBSITE_TIMEZONE = process.env.TZ || "Europe/Brussels";
-export const WEBSITE_LOCALE = process.env.WEBSITE_LOCALE || "en-BE";
+// Don't set defaults automatically when testing
+if (process.env.NODE_ENV != "test") {
+    config();
+}
+
+/**
+ * The keys here are in line with the environment variables,
+ * not with internal code!
+ * 
+ * @example conf.WEBSITE_TIMEZONE = process.env.TZ || _DEFAULTS.TZ
+ */
+export const _DEFAULTS = {
+    HOST_DOMAIN: "localhost:3000",
+    DATA_DIR: "data/",
+    WEBSITE_TITLE: "Community Pinboard!",
+    WEBSITE_DESCRIPTION: "A public event pinboard for your local community!",
+    TZ: "Europe/Brussels",
+    WEBSITE_LOCALE: "nl-BE",    
+    MAX_TITLE: 80,
+    MAX_DESCRIPTION: 400,
+    MAX_LOCATION: 150,
+    MAX_POSTEDBY: 50,
+    MAX_THUMBNAILURL: 50,
+    MAX_UPLOAD_MB: 20
+}
+
+export const HOST_DOMAIN = process.env.HOST_DOMAIN || _DEFAULTS.HOST_DOMAIN;
+export const DATA_DIR = process.env.DATA_DIR || _DEFAULTS.DATA_DIR;
+export const WEBSITE_TITLE = process.env.WEBSITE_TITLE || _DEFAULTS.WEBSITE_TITLE;
+export const WEBSITE_DESCRIPTION = process.env.WEBSITE_DESCRIPTION || _DEFAULTS.WEBSITE_DESCRIPTION;
+export const WEBSITE_TIMEZONE = process.env.TZ || _DEFAULTS.TZ;
+export const WEBSITE_LOCALE = process.env.WEBSITE_LOCALE || _DEFAULTS.WEBSITE_LOCALE;
 
 export const PIN_MAXLENGTHS = {
-    "title": process.env.MAX_TITLE ? parseInt(process.env.MAX_TITLE) : 80,
-    "description": process.env.MAX_DESCRIPTION ? parseInt(process.env.MAX_DESCRIPTION) : 400,
-    "location": process.env.MAX_LOCATION ? parseInt(process.env.MAX_LOCATION) : 150,
-    "postedBy": process.env.MAX_POSTEDBY ? parseInt(process.env.MAX_POSTEDBY) : 50,
-    "thumbnailUrl": process.env.MAX_THUMBNAILURL ? parseInt(process.env.MAX_THUMBNAILURL) : 50
+    "title": process.env.MAX_TITLE ? parseInt(process.env.MAX_TITLE) : _DEFAULTS.MAX_TITLE,
+    "description": process.env.MAX_DESCRIPTION ? parseInt(process.env.MAX_DESCRIPTION) : _DEFAULTS.MAX_DESCRIPTION,
+    "location": process.env.MAX_LOCATION ? parseInt(process.env.MAX_LOCATION) : _DEFAULTS.MAX_LOCATION,
+    "postedBy": process.env.MAX_POSTEDBY ? parseInt(process.env.MAX_POSTEDBY) : _DEFAULTS.MAX_POSTEDBY,
+    "thumbnailUrl": process.env.MAX_THUMBNAILURL ? parseInt(process.env.MAX_THUMBNAILURL) : _DEFAULTS.MAX_THUMBNAILURL
 };
-export const MAX_UPLOAD_MB = process.env.MAX_UPLOAD_MB ? parseInt(process.env.MAX_UPLOAD_MB) : 20
+export const MAX_UPLOAD_MB = process.env.MAX_UPLOAD_MB ? parseInt(process.env.MAX_UPLOAD_MB) : _DEFAULTS.MAX_UPLOAD_MB
 
 
 export const PINS_DIR = DATA_DIR + "pins/";
